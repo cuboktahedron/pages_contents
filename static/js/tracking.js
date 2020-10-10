04daacfaf88494105edf835f26ed3ee5836f227c
cuboktahedron <cuboktahedron@gmail.com>
2020-10-10 20:33:02 +0900

    Fix to be tracked properly

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -1,13 +1,17 @@
-var ga = document.createElement('script');
-ga.type = 'text/javascript';
-ga.async = true;
-ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-26499443-2';
-
-var head = document.querySelector('head');
-head.prepend(ga);
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+Promise.all([
+    new Promise((resolve) => {
+        var script = document.createElement('script');
+        script.src = "https://www.googletagmanager.com/gtag/js?id=UA-26499443-2";
+        script.async = true;
+        document.head.appendChild(script);
+        resolve();
+    }),
+    new Promise((resolve) => {
+        gtag('js', new Date());
 
-gtag('config', 'UA-26499443-2');
+        gtag('config', 'UA-26499443-2');
+        resolve();
+    })
+]);
