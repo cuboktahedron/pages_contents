beb42a9b16264504c13634f614272b2c83a8b08b
cuboktahedron <cuboktahedron@gmail.com>
2020-10-10 17:59:00 +0900

    Upgrade tracking tag

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -1,9 +1,13 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+var ga = document.createElement('script');
+ga.type = 'text/javascript';
+ga.async = true;
+ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-26499443-2';
 
-ga('create', 'UA-26499443-2', 'auto');
-ga('send', 'pageview');
+var head = document.querySelector('head');
+head.prepend(ga);
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
+gtag('config', 'UA-26499443-2');
